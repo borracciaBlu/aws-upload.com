88601c4fedc42d416d8cad7e328f232ec25f7815
borracciaBlu <marco.asdman@gmail.com>
2017-07-15 14:19:01 +1000

    [ADD] First commit. Import website source.

---
+++ Gruntfile.js
@@ -0,0 +1,135 @@
+module.exports = function(grunt) {
+    require('time-grunt')(grunt);
+
+    // Tasks loaded
+    grunt.loadNpmTasks('grunt-inline');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // @grunt-contrib-clean
+        // clean folders
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        'dist/*',
+                        '!dist/.git*'
+                    ]
+                }]
+            },
+            inline: {
+                files: [{
+                    src: [
+                        'dist/css/inline-*.css',
+                    ]
+                }]  
+            }
+        },
+
+        // @grunt-contrib-less
+        // compile less ~> css
+        less: {
+            compile: {
+                options: {
+                    cleancss: true,
+                    strictImports : true
+                },
+                files: {
+                    'src/css/main.css': 'src/less/main.less',
+                    'src/css/inline-doc.css': 'src/less/inline-doc.less',
+                    'src/css/inline-home.css': 'src/less/inline-home.less',
+                }
+            }
+        },
+
+        // @grunt-postcss
+        // +~> @cssnano
+        // +~> @autoprefixer
+        // minify css
+        // add browser prefixes
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({browsers: ['last 1 version']}),
+                    require('cssnano')
+                ]
+            },
+            dist: {
+                src: 'src/css/*.css'
+            }
+        },
+
+        // @grunt-contrib-copy
+        // move files src/ ~> dist/
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'src',
+                    dest: 'dist',
+                    src: [
+                        '*.{ico,png,txt}',
+                        '*.html',
+                        'css/*',
+                        'images/*',
+                        'favicons/*',
+                    ]
+                }]
+            }
+        },
+
+        // @grunt-inline
+        // put css inline in html
+        inline: {
+            dist: {
+                options: {
+                    inlineTagAttributes: {
+                        css: 'data-inlined="true"'
+                    },
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/guide.html': 'dist/guide.html',
+                }
+            }
+        },
+
+        // @grunt-contrib-watch
+        // use livereload
+        watch: {
+            reload: {
+                files: ['Gruntfile.js',
+                        "src/*"],
+                options: {
+                    livereload: true
+                }
+            },
+            less: {
+                files: ['src/less/*', 'src/less/*/*'],
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
+            },
+        },
+    });
+
+    grunt.registerTask('css', ['less', 'postcss']);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'css',
+        'copy:dist',
+        'inline:dist',
+        'clean:inline'
+    ]);
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# aws-upload.com
+The website.
